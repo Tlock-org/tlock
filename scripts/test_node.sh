19e3afa6dc9eb8781a539b1c6c2fa8545da06e1c
Nathaniel-Watler <NathanielWatler@outlook.com>
2024-11-20 10:34:08 +0800

    feegrant

--- scripts/test_node.sh
+++ scripts/test_node.sh
@@ -84,15 +84,15 @@ from_scratch () {
   update_test_genesis '.app_state["tokenfactory"]["params"]["denom_creation_gas_consume"]=100000'
 
    # set TOK decimal
-  update_test_genesis '.app_state["bank"]["denom_metadata"] += [{
-    "description": "Tlock token",
-    "DenomUnits": "[]*banktypes.DenomUnit{
-                        {Denom: "uTOK", Exponent: 0, Aliases: []string{"microTOK"}},
-                        {Denom: "TOK", Exponent: 6},
-                    }",
-    "display": "TOK",
-    "base": "uTOK",
-  }]'
+#  update_test_genesis '.app_state["bank"]["denom_metadata"] += [{
+#    "description": "Tlock token",
+#    "DenomUnits": "[]*banktypes.DenomUnit{
+#                        {Denom: "uTOK", Exponent: 0, Aliases: []string{"microTOK"}},
+#                        {Denom: "TOK", Exponent: 6},
+#                    }",
+#    "display": "TOK",
+#    "base": "uTOK",
+#  }]'
 
   # Allocate genesis accounts
   BINARY genesis add-genesis-account $KEY 10000000000000000000$DENOM --keyring-backend $KEYRING --append
